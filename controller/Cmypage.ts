25dd8239fa3964aef2b745003c01c38b54121f24
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-08-30 03:18:46 +0900

    Comment : Community api need comment

--- controller/Cmypage.ts
+++ controller/Cmypage.ts
@@ -6,6 +6,7 @@ import url from 'url';
 
 dotenv.config();
 
+//testapi 아직 개발되지 않은 api의 endpoint, 요청이 제대로 도달하는지 확인 가능
 export const testApi = async (req: Request, res: Response) => {
   try {
     console.log('req >>>> ', req);  
--- controller/Csearch.ts
+++ controller/Csearch.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+//testapi 아직 개발되지 않은 api의 endpoint, 요청이 제대로 도달하는지 확인 가능
 export const testApi = async (req: Request, res: Response) => {
   try {
     console.log('req >>>> ', req);
